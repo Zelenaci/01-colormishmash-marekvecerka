8eb765bce0cdb54f0da98590d9f0b06071e35d9a
marekvecerka <vec38035@spseol.cz>
2021-11-29 09:28:48 +0100

    zacatek ve skole

--- main.py
+++ main.py
@@ -2,40 +2,47 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import Scale, HORIZONTAL
 
 # from tkinter import ttk
 
 
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
-
-
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "ColorMishMash"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
+
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
+
+        self.lblR = tk.Label(self, text="R")
+        self.lblR.pack()
+        self.scaleR = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleR.pack()
+
+        self.lblG = tk.Label(self, text="G")
+        self.lblG.pack()
+        self.scaleG = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleG.pack()
+
+        self.lblB = tk.Label(self, text="B")
+        self.lblB.pack()
+        self.scaleB = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleB.pack()
+
+        self.btnQuit = tk.Button(self, text="Quit", command=self.quit)
+        self.btnQuit.pack()
+
+        self.btn2 = tk.Button(self, text="About", command=self.quit)
         self.btn2.pack()
 
-    def about(self):
-        window = About(self)
-        window.grab_set()
+    def change(self):
+        r = self.scaleR.get()
+        g = self.scaleR.get()
+        b = self.scaleR.get()
+        self.CanvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
 
     def quit(self, event=None):
         super().quit()
