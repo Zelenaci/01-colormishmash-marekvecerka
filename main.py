709021aa01210e4e7ce5014cf7bb2efe74d67bcd
marekvecerka <93651571+marekvecerka@users.noreply.github.com>
2021-12-12 17:32:39 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Scale, HORIZONTAL
+from tkinter import Label, Button, Scale, HORIZONTAL, Canvas
 
 # from tkinter import ttk
 
@@ -10,7 +10,7 @@ from tkinter import Scale, HORIZONTAL
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
     name = "ColorMishMash"
-
+    
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
@@ -19,30 +19,38 @@ class Application(tk.Tk):
 
         self.lblR = tk.Label(self, text="R")
         self.lblR.pack()
-        self.scaleR = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleR = Scale(from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change)
         self.scaleR.pack()
 
         self.lblG = tk.Label(self, text="G")
         self.lblG.pack()
-        self.scaleG = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleG = Scale(from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change)
         self.scaleG.pack()
 
         self.lblB = tk.Label(self, text="B")
         self.lblB.pack()
-        self.scaleB = Scale(from_=0, to=255, orient=HORIZONTAL, length=256)
+        self.scaleB = Scale(from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change)
         self.scaleB.pack()
+        
+
+        self.canvasMain = Canvas(width=256, height=100, background="#123456")
+        self.canvasMain.pack()
+
+
 
         self.btnQuit = tk.Button(self, text="Quit", command=self.quit)
         self.btnQuit.pack()
 
-        self.btn2 = tk.Button(self, text="About", command=self.quit)
-        self.btn2.pack()
 
-    def change(self):
+
+    def change(self, event):
+
         r = self.scaleR.get()
-        g = self.scaleR.get()
-        b = self.scaleR.get()
-        self.CanvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
+        g = self.scaleG.get()
+        b = self.scaleB.get()
+        self.canvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
+        print(f"#{r:2x}{g:2x}{b:2x}")
+        print(f"#{r:02x}{g:02x}{b:02x}")
 
     def quit(self, event=None):
         super().quit()
